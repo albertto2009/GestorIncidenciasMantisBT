ea40da77d908d0b27a729bf418a13a4146e6b727
unknown <alberto_villa95@hotmail.com>
2021-05-10 19:00:54 +0200

    Primera Version

---
+++ var/www/html/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysqli';
+$g_database_name = 'mantisbt';
+$g_db_username = 'mantis';
+$g_db_password = 'toor';
+$g_default_language = 'spanish';
+$g_logo_image = 'images/images.png';
+$g_allow_signup	= OFF;
+$g_default_bug_view_status = VS_PRIVATE;
+$g_file_upload_max_num = 2;
